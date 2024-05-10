81ea723e1784e5b4faf98c0e04d9833573df75f5
ShiramiSK <81412720+ShiramiSK@users.noreply.github.com>
2024-05-10 21:34:57 +0200

    Footer added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,14 @@ import '../../App.css'
 import Header from "../Header";
 
 import Articles from "../Articles";
+import Footer from "../Footer";
 
 function Home(){
     return(
         <>
             <Header/>
             <Articles/>
+            <Footer/>
         </>
     );
 }
