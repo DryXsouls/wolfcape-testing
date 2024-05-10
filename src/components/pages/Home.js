a2a1e4089a78fb69ea948c9625e7c8152d1b35a8
DryXsouls <cigana2tahy@gmail.com>
2024-05-10 21:41:43 +0200

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,15 @@
 import React from "react";
 import '../../App.css'
 import Header from "../Header";
-
 import Articles from "../Articles";
+import Footer from "../Footer";
 
 function Home(){
     return(
         <>
             <Header/>
             <Articles/>
+            <Footer/>
         </>
     );
 }
